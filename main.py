de49bfbe477c29443100ebb7652ddacabea48a50
Zielonka <zielonkae6@rowan.edu>
2018-11-15 14:21:37 -0500

    basic functionality for naming columns, clean code a bit

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ import plotly.offline as pl
 import plotly.graph_objs as go
 
 class ImportCSVFrame:
-    def __init__(self, parent):
+    def __init__(self, parent, **kwargs):
         self.parent = parent
 
-        self.frame = ttk.Frame(root, padding="5")
+        self.frame = ttk.Frame(self.parent, padding="5")
         self.frame.grid(column=0, row=0, sticky=(N,S,E,W))
         self.frame.columnconfigure(2, weight=1)
         self.frame.rowconfigure(5, weight=1)
@@ -36,7 +36,52 @@ class ImportCSVFrame:
         data = pd.read_csv(self.filename.get(), header=(0 if self.headerInFile.get() else None), prefix="col-")
         
         # transition to next frame
-        ChooseColumnsFrame(self.parent, data=data)
+        NameColumnsFrame(self.parent, data=data)
+        self.frame.destroy()
+        return
+
+class NameColumnsFrame:
+    def __init__(self, parent, **kwargs):
+        self.parent = parent
+        self.data = kwargs["data"]
+
+        self.frame = ttk.Frame(self.parent, padding="5")
+        self.frame.grid(column=0, row=0, sticky=(N,S,E,W))
+        self.frame.columnconfigure(1, weight=1)
+        self.frame.rowconfigure(1, weight=1)
+
+        ## setup variables
+        self.originalColumnNames = list(self.data.columns.values)
+        self.newColumnNames = []
+
+        ## GUI Components
+        ttk.Label(self.frame, text="Rename the columns (optional)").grid(column=0, row=0, sticky=(N,W))
+        innerFrame = ttk.Frame(self.frame, padding="5")
+        innerFrame.grid(column=0, row=1, columnspan=3, sticky=(N,W,E,S))
+        ttk.Button(self.frame, text="Next", command=self.process).grid(column=3, row=3, sticky=E)
+
+        self.populateFrame(innerFrame)
+
+    ### functions
+
+    def populateFrame(self, frame, **kwargs):
+        for i, column in enumerate(self.originalColumnNames):
+            var = StringVar()
+            var.set(column)
+            self.newColumnNames.append(var)
+
+            ttk.Entry(frame, textvariable=var).grid(column=i, row=0, padx=10, pady=4)
+
+
+        return
+
+    def process(self, **kwargs):
+        #rename the columns in `data`
+        newColumnNamesList = [name.get() for name in self.newColumnNames]
+        self.data.rename(columns=dict(zip(self.originalColumnNames, newColumnNamesList)), inplace=True)
+
+        # transition to next frame
+        ChooseColumnsFrame(self.parent, data=self.data)
         self.frame.destroy()
         return
 
@@ -45,7 +90,7 @@ class ChooseColumnsFrame:
         self.parent = parent
         self.data = kwargs["data"]
 
-        self.frame = ttk.Frame(root, padding="5")
+        self.frame = ttk.Frame(self.parent, padding="5")
         self.frame.grid(column=0, row=0, sticky=(N,S,E,W))
         
         ## GUI Components
@@ -62,7 +107,7 @@ class MakeGraphFrame:
         self.parent = parent
         self.data = kwargs["data"]
 
-        self.frame = ttk.Frame(root, padding="5")
+        self.frame = ttk.Frame(self.parent, padding="5")
         self.frame.grid(column=0, row=0, sticky=(N,S,E,W))
         
         ## GUI Components
