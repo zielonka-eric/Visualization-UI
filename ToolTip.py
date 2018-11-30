053ecf0bbc668e8657fb158d4c037b3186e155dc
Deep Desai <44065733+desaid64@users.noreply.github.com>
2018-11-30 11:04:27 -0500

    Add files via upload

---
+++ ToolTip.py
@@ -0,0 +1,40 @@
+#Authors: Matthew Schofield
+import tkinter as tk
+#from main.gui.Utilities.Settings import Settings
+
+'''
+A Tooltip to be bound to GUI widgets to supply extra information
+'''
+class ToolTip:
+
+    '''
+    Binds the tooltip to a given widget with supplied text
+
+    :param tk_widget, widget to add tooltip to
+    :param info, info to have on tooltip
+    '''
+    def __init__(self, tk_widget, info):
+        self.tk_widget = tk_widget
+        self.tk_widget.bind("<Enter>", self.show)
+        self.tk_widget.bind("<Leave>", self.destroy)
+        self.info = info
+
+    '''
+    Show the widget upon mosueover
+    '''
+    def show(self, event=None):
+        x, y, _, __ = self.tk_widget.bbox("insert")
+        x += self.tk_widget.winfo_rootx() + 25
+        y += self.tk_widget.winfo_rooty() + 20
+        self.tw = tk.Toplevel(self.tk_widget)
+        self.tw.wm_overrideredirect(True)
+        self.tw.wm_geometry("+%d+%d" % (x, y))
+        label = tk.Label(self.tw, text=self.info, borderwidth=2,relief="groove", background="ghost white")
+        label.pack(ipadx=1)
+
+    '''
+    Cleans up after itself when mouse pointer leaves
+    '''
+    def destroy(self, event=None):
+        if self.tw:
+            self.tw.destroy()
